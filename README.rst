ed374bf1652e71f137aafa2b323d75f969bc3a68
leplatrem <contact@mathieu-leplatre.info>
2011-04-18 14:54:29 +0200

    Initial version

---
+++ README.rst
@@ -0,0 +1,61 @@
+EasyDict
+########
+
+Access dict values as attributes (works recursively)
+
+<https://github.com/leplatrem/easydict>
+
+=====
+USAGE
+=====
+
+.. code-block :: python
+
+    >>> from easydict import EasyDict
+    >>> d = EasyDict({'foo':3, 'bar':{'x':1, 'y':2}})
+    >>> d.foo
+    3
+    >>> d.bar.x
+    1
+
+Very useful when exploiting parsed JSON content ! 
+
+.. code-block :: python
+
+    >>> from easydict import EasyDict
+    >>> from simplejson import loads
+    >>> j = """{
+    "Buffer": 12,
+    "List1": [
+        {"type" : "point", "coordinates" : [100.1,54.9] },
+        {"type" : "point", "coordinates" : [109.4,65.1] },
+        {"type" : "point", "coordinates" : [115.2,80.2] },
+        {"type" : "point", "coordinates" : [150.9,97.8] }
+    ]
+    }"""
+    >>> d = EasyDict(loads(j))
+    >>> d.Buffer
+    12
+    >>> d.List1[0].coordinates[1]
+    54.9
+
+
+=======
+LICENSE
+=======
+
+* Lesser GNU Public License
+
+=========
+CHANGELOG
+=========
+
+1.0
+---
+* Initial version. Does what I need.
+
+=======
+AUTHORS
+=======
+
+* Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
---
+++ easydict/__init__.py
@@ -0,0 +1,41 @@
+class EasyDict(dict):
+    """
+    >>> d = EasyDict({'foo':3})
+    >>> d['foo']
+    3
+    >>> d.foo
+    3
+    >>> d.bar
+    Traceback (most recent call last):
+    ...
+    AttributeError: 'EasyDict' object has no attribute 'bar'
+    >>> d = EasyDict({'foo':3, 'bar':{'x':1, 'y':2}})
+    >>> isinstance(d.bar, dict)
+    True
+    >>> d.bar.x
+    1
+    >>> d = EasyDict({'foo':0, 'bar':[{'x':1, 'y':2}, {'x':3, 'y':4}]})
+    >>> isinstance(d.bar, list)
+    True
+    >>> from operator import attrgetter
+    >>> map(attrgetter('x'), d.bar)
+    [1, 3]
+    >>> map(attrgetter('y'), d.bar)
+    [2, 4]
+    """
+    def __init__(self, d):
+        for k, v in d.items():
+            setattr(self, k, v)
+
+    def __setattr__(self, name, value):
+        if isinstance(value, (list, tuple)):
+            value = [EasyDict(x) if isinstance(x, dict) else x for x in value]
+        else:
+            value = EasyDict(value) if isinstance(value, dict) else value
+        super(EasyDict, self).__setattr__(name, value)
+        self[name] = value
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
