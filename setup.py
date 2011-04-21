5071755f84acf0ac378196ba00e4854928d10e35
leplatrem <contact@mathieu-leplatre.info>
2011-04-21 15:32:25 +0200

    switch to distutils (fixes #1)

--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
 import os
-from setuptools import setup, find_packages
+from distutils.core import setup
 
 here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
-    name = "easydict",
-    version = "1.0",
-    description="Access dict values as attributes (works recursively)",
-    long_description=open(os.path.join(here, 'README.rst')).read(),
-    packages = find_packages(),
+    name='easysdict',
+    version='1.0',
+    author='Mathieu Leplatre',
+    author_email='mathieu.leplatre@makina-corpus.com',
+    packages=['easydict'],
+    url='https://github.com/makinacorpus/easydict',
     classifiers  = ['Topic :: Utilities', 
                     'Natural Language :: English',
                     'Operating System :: OS Independent',
                     'Intended Audience :: Developers',
                     'Development Status :: 5 - Production/Stable',
                     'Programming Language :: Python :: 2.5'],
-    author = 'Mathieu Leplatre',
-    author_email = 'mathieu.leplatre@makina-corpus.com',
+    description="Access dict values as attributes (works recursively)",
+    long_description=open(os.path.join(here, 'README.rst')).read(),
 )
+
