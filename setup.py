1f789ef149cc30e17c7fef47ba0c07be7c17ba0e
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2014-08-07 18:48:00 +0200

    Preparing release 1.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
     name='easydict',
-    version='1.4',
+    version='1.5',
     author='Mathieu Leplatre',
     author_email='mathieu.leplatre@makina-corpus.com',
     url='https://github.com/makinacorpus/easydict',
