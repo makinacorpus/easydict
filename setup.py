330875a40c4d85ff8a8ba94a38b9f9efe8ecf14a
Mathieu Leplatre <mathieu@mozilla.com>
2023-10-24 00:56:43 +0200

    Remove inadequate keywords

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    keywords=['MBTiles', 'Mapnik'],
+    keywords=['Tools'],
     classifiers=['Topic :: Utilities', 
                  'Natural Language :: English',
                  'Operating System :: OS Independent',
