2abe1f358d97665c640508a5aadae54c29d45819
Mathieu Leplatre <mathieu@mozilla.com>
2024-02-13 10:01:03 +0100

    Run lint and tests from GHA

--- easydict/__init__.py
+++ easydict/__init__.py
@@ -135,7 +135,7 @@ class EasyDict(dict):
             setattr(self, k, v)
         # Class attributes
         for k in self.__class__.__dict__.keys():
-            if not (k.startswith('__') and k.endswith('__')) and not k in ('update', 'pop'):
+            if not (k.startswith('__') and k.endswith('__')) and k not in ('update', 'pop'):
                 setattr(self, k, getattr(self, k))
 
     def __setattr__(self, name, value):
